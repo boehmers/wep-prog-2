9bd9ec878ed7d04a7bcd7a2d340ff513dfac635e
wutcru <dukenni62255@th-nuernberg.de>
2016-05-30 11:14:11 +0200

    Userdata, Logout, User-Session, Login Party

--- src/js/userdata.js
+++ src/js/userdata.js
@@ -1,59 +1,44 @@
 /**
  * Created by PaoloPinkel on 26.05.2016.
  */
-function writeJSON(filename, content) {
-    var request = new XMLHttpRequest();
-    request.open("GET", "../php/savefile.php?filename=../../data/" + filename + "&content=" + content, false);
-    request.send();
-    console.log("Write done!");
-};
-
-function readFile(filename) {
-    var request = new XMLHttpRequest();
-    request.open("GET", "../php/readfile.php?filename=../../data/" + filename, false);
-    request.send();
-    console.log("Read done!");
-    console.log(request.responseText);
-    return request.responseText;
-};
 
 function register(username, password, colour){
     var tempJson = JSON.parse(readFile("example.json")); //look up for names that already taken
     var i = 0;
-    for(i; i < tempJson.user.length; i++){
-        if(tempJson.user[i][i] === username){
+    for(i; i < tempJson.length; i++){
+        if(tempJson[i].user === username){
             window.alert("This name already exist");
             return;
         }
     }
     //no existence of this username...
     var usern = new Object();
-    var pw = new Object();
-    var col = new Object();
-    usern[i] = username;
-    pw[i] = password;
-    col[i] = colour ;
+    usern["user"] = username;
+    usern["pw"] = password;
+    usern["color"] = colour;
     
-    tempJson.user.push(usern);
-    tempJson.dwp.push(pw);
-    tempJson.colo.push(col);
+    tempJson.push(usern);
 
     var writeDown = JSON.stringify(tempJson);
-    writeJSON("example.json", writeDown);
+    writeFile("example.json", writeDown);
 }
 
 function loginCheck(){
     var usern = document.getElementById("username").value;
     var pwd = document.getElementById("password").value;
+
     var tempJson = JSON.parse(readFile("example.json")); //look up for names that already taken
     var i = 0;
-    for(i; i < tempJson.user.length; i++){
-        if((tempJson.user[i][i] === usern) && (tempJson.dwp[i][i] === pwd)){
+    for(i; i < tempJson.length; i++){
+        if((tempJson[i].user === usern) && (tempJson[i].pw === pwd)){
             //add code for successful login session
             window.alert("Success!!!!11111oneeleven");
-            window.open("http://localhost/wep-prog-2/src/html/main.html?login=" + usern + "&colour=" + tempJson.colo[i][i],"_self");
+            window.open("../html/main.html?login=" + usern + "&colour=" + tempJson[i].color,"_self");
+            return;
         }
     }
+
+    window.alert("User/Passwort nicht vorhanden.");
 }
 
 //Displays Username in Colour
