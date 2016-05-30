7e0a156bfc081a1bbfb279c37d4323aa36f1498e
markusgl <markus.glas@gmx.net>
2016-05-30 18:51:41 +0200

    Details Button wird zur Taskbox hinzugefügt

--- src/js/task.js
+++ src/js/task.js
@@ -4,33 +4,46 @@
 var modal;
 var counter=0;
 var taskTitle;
+var taskDescription;
+var detailsButton;
 var node;
 
 //noinspection JSAnnotator
-function add(listID){
+function add(listID) {
     //adds a taskBox to the tasklist
     counter++;
     node = document.createElement("DIV");
+    detailsButton = document.createElement("DIV");
     var taskBoxLabel = document.createElement("LABEL");
+
+    detailsButton.innerHTML = "Details...";
+    detailsButton.className = "button";
+    detailsButton.id = "detailsButton"+counter;
+    detailsButton.addEventListener('click', modalDialog('details'));
+
     taskBoxLabel.className="taskBoxLabel";
     taskBoxLabel.id="taskLabel"+counter;
     node.className="task";  //for CSS
     node.id="task"+counter;
-    //console.log(node.id);
 
-    //make taskbox draggable and append children    
+    //make taskbox draggable and append children 
     node.draggable="true";
     node.addEventListener("dragstart", drag);
     document.getElementById(listID).appendChild(node);
     node.appendChild(taskBoxLabel);
+    node.appendChild(detailsButton);
+
 };
 
-function modalDialog(){
+function modalDialog(element){
     //show modal dialog
-    modal = document.getElementById('modalDialog');
-    modal.style.display="block";
+    modal = document.getElementById(element);
+    modal.style.display = "block";
     taskTitle = document.getElementById('taskTitle');
-    taskTitle.value="";
+    taskTitle.value = "";
+    taskDescription = document.getElementById('taskDescription');
+    taskDescription.value = "";
+
 };
 
 function cancel(){
@@ -51,3 +64,5 @@ function save(){
     //    node.style.height="10%";
     //}
 };
+
+
