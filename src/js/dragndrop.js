03cbed9ea4681b5461e05af786445e23a785c2f7
Manuel <manuelboehm1@t-online.de>
2016-05-30 09:54:35 +0200

    resolve conflicts

--- src/js/dragndrop.js
+++ src/js/dragndrop.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /**
  * Created by Manuel on 25.05.2016.
  */
@@ -14,21 +13,4 @@ function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
-=======
-/**
- * Created by Manuel on 25.05.2016.
- */
-function allowDrop(ev) {
-    ev.preventDefault();
-}   
-
-function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
-}
-
-function drop(ev) {
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
->>>>>>> 8d4004d67a7d9f410a11da3592a7e996f17ff286
 }
--- src/js/fileaccess.js
+++ src/js/fileaccess.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function writeFile(filename, content) {
 	var request = new XMLHttpRequest();
 	request.open("GET", "../php/savefile.php?filename=../../data/" + filename + "&content=" + content, false);
@@ -44,51 +43,4 @@ function TESTOnDoneRead(text) {
 
 function TESTOnDoneWrite() {
 	console.log("Async Write done!");
-=======
-function writeFile(filename, content) {
-	var request = new XMLHttpRequest();
-	request.open("GET", "../php/savefile.php?filename=../../data/" + filename + "&content=" + content, false);
-    request.send();
-	console.log("Write done!");
-}
-
-function writeFileAsync(filename, content, onDone) {
-	var request = new XMLHttpRequest();
-	request.onreadystatechange = function(){
-			if (request.readyState == XMLHttpRequest.DONE) {
-				onDone();
-			}
-		};
-	request.open("GET", "../php/savefile.php?filename=../../data/" + filename + "&content=" + content, true);
-    request.send();
-}
-
-function readFile(filename) {
-	var request = new XMLHttpRequest();
-	request.open("GET", "../php/readfile.php?filename=../../data/" + filename, false);
-    request.send();
-	console.log("Read done!");
-	console.log(request.responseText);
-	return request.responseText;
-}
-
-function readFileAsync(filename, onDone) {
-	var request = new XMLHttpRequest();
-	request.onreadystatechange = function(){
-			if (request.readyState == XMLHttpRequest.DONE) {
-				onDone(request.responseText);
-			}
-		};
-	request.open("GET", "../php/readfile.php?filename=../../data/" + filename, true);
-    request.send();
-}
-
-function TESTOnDoneRead(text) {
-	console.log("Aysnc Read done!");
-	console.log(text);
-}
-
-function TESTOnDoneWrite() {
-	console.log("Async Write done!");
->>>>>>> 8d4004d67a7d9f410a11da3592a7e996f17ff286
 }
